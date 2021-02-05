5a21b178ada8cab2b7e411e6806d3e820b158bce
Syarif <syarifuddin.studio@gmail.com>
2021-02-05 08:41:09 +0800

    using theoplayer demo

---
+++ precache-manifest.063f52b5e93abda63ff26dac59f05e07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d60f726834b0f14d7edd1549b647dfde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "480886a673c5119b4575",
+    "url": "/static/css/main.2ba960e9.chunk.css"
+  },
+  {
+    "revision": "8bbb04e4f3317fdb1dfc",
+    "url": "/static/js/2.1d79e655.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1d79e655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "480886a673c5119b4575",
+    "url": "/static/js/main.3effa77b.chunk.js"
+  },
+  {
+    "revision": "f4ed6260bbc5d6e8fb31",
+    "url": "/static/js/runtime-main.b0844db5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
